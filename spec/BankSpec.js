4053772e7e6cc1b47171cdff70a0c4f7509dc492
jeff1108 <jeffhongkong2007@gmail.com>
2018-08-14 16:34:42 +0100

    OTHER Create the debt and credit amount in the bank statement

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -3,10 +3,10 @@ describe("Bank", () => {
 
   beforeEach(function() {
     bank = new Bank();
+    jasmine.clock().install()
     var baseTime = new Date(2018, 7, 15)
-    jasmine.clock().install();
-    jasmine.clock().mockDate(baseTime);
-    jasmine.clock().tick(50);
+    jasmine.clock().mockDate(baseTime)
+    jasmine.clock().tick(50)
   });
 
   afterEach(function() {
@@ -25,21 +25,20 @@ describe("Bank", () => {
     });
 
     it("can record to my bank statement when deposit", () => {
-      bank.deposit(1000);
-      expect(bank._bank_statement).toEqual([{current_time: "15/8/2018", amount: 1000, balance: 1000}]);
+      bank.deposit(1000)
+      expect(bank._bank_statement).toEqual([{current_time: "15/8/2018", debt: 1000, credit: 0, balance: 1000}])
     });
   });
 
   describe(".withdrawal", () => {
     it("can get the moeny", () => {
-      bank.deposit(1000);
+      bank.deposit(1000)
       expect(bank.withdrawal(500)).toEqual(500);
     });
-    
     it("can record to my bank statement when withdrawal", () => {
-      bank.deposit(1000);
-      bank.withdrawal(500);
-      expect(bank._bank_statement).toEqual([{current_time: "15/8/2018", amount: 1000, balance: 1000}, {current_time: "15/8/2018", amount: 500, balance: 500}]);
+      bank.deposit(1000)
+      bank.withdrawal(500)
+      expect(bank._bank_statement).toEqual([{current_time: "15/8/2018", debt: 1000, credit: 0, balance: 1000}, {current_time: "15/8/2018", debt: 0, credit: 500, balance: 500}])
     });
   });
 });
