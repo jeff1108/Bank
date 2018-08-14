74dfc354ebbba484b226027ed416dddd1b87fc1c
jeff1108 <jeffhongkong2007@gmail.com>
2018-08-14 14:36:07 +0100

    FEATURE Create record and time methods

--- src/Bank.js
+++ src/Bank.js
@@ -1,12 +1,29 @@
 var Bank = function() {
   this._balance = 0;
+  this._bank_statement = [];
 
 }
 
 Bank.prototype.deposit = function(amount) {
-  return this._balance += amount
+  this._balance += amount
+  this.record(amount)
+  return this._balance;
 }
 
 Bank.prototype.withdrawal = function(amount) {
   return this._balance -= amount;
+}
+
+Bank.prototype.record = function(amount) {
+  var current_time = this.time()
+  var balance = this._balance
+  this._bank_statement.push({current_time, amount, balance});
+}
+
+Bank.prototype.time = function() {
+  time = new Date();
+  day = time.getDate()
+  month = time.getMonth()+1
+  year = time.getFullYear()
+  return current_time = day + "/" + month + "/" + year
 }
