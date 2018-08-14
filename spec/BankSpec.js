f294b8c1b8a00640ea7cfbf861c3222b65659590
jeff1108 <jeffhongkong2007@gmail.com>
2018-08-14 14:44:54 +0100

    TEST Record bank transaction when withdrawal in bank

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -4,14 +4,14 @@ describe("Bank", () => {
   beforeEach(function() {
     bank = new Bank();
     var baseTime = new Date(2018, 7, 15)
-    jasmine.clock().install()
-    jasmine.clock().mockDate(baseTime)
-    jasmine.clock().tick(50)
+    jasmine.clock().install();
+    jasmine.clock().mockDate(baseTime);
+    jasmine.clock().tick(50);
   });
 
   afterEach(function() {
-  jasmine.clock().uninstall();
-});
+    jasmine.clock().uninstall();
+  });
 
   describe(".balance", () => {
     it("start at 0 ", () => {
@@ -24,18 +24,22 @@ describe("Bank", () => {
       expect(bank.deposit(1000)).toEqual(1000);
     });
 
-    it("can record to my bank statement", () => {
-      bank.deposit(1000)
-      expect(bank._bank_statement).toEqual([{current_time: "15/8/2018", amount: 1000, balance: 1000}])
+    it("can record to my bank statement when deposit", () => {
+      bank.deposit(1000);
+      expect(bank._bank_statement).toEqual([{current_time: "15/8/2018", amount: 1000, balance: 1000}]);
     });
   });
 
   describe(".withdrawal", () => {
     it("can get the moeny", () => {
-      bank.deposit(1000)
+      bank.deposit(1000);
       expect(bank.withdrawal(500)).toEqual(500);
     });
+    
+    it("can record to my bank statement when withdrawal", () => {
+      bank.deposit(1000);
+      bank.withdrawal(500);
+      expect(bank._bank_statement).toEqual([{current_time: "15/8/2018", amount: 1000, balance: 1000}, {current_time: "15/8/2018", amount: 500, balance: 500}]);
+    });
   });
-
-
 });
--- src/Bank.js
+++ src/Bank.js
@@ -5,8 +5,8 @@ var Bank = function() {
 }
 
 Bank.prototype.deposit = function(amount) {
-  this._balance += amount
-  this.record(amount)
+  this._balance += amount;
+  this.record(amount);
   return this._balance;
 }
 
@@ -15,15 +15,15 @@ Bank.prototype.withdrawal = function(amount) {
 }
 
 Bank.prototype.record = function(amount) {
-  var current_time = this.time()
-  var balance = this._balance
+  var current_time = this.time();
+  var balance = this._balance;
   this._bank_statement.push({current_time, amount, balance});
 }
 
 Bank.prototype.time = function() {
   time = new Date();
-  day = time.getDate()
-  month = time.getMonth()+1
-  year = time.getFullYear()
-  return current_time = day + "/" + month + "/" + year
+  day = time.getDate();
+  month = time.getMonth()+1;
+  year = time.getFullYear();
+  return current_time = day + "/" + month + "/" + year;
 }
