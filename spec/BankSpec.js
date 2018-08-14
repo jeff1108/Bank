3fd6a82427c8cfa1ffef65797733ddf61057a583
jeff1108 <jeffhongkong2007@gmail.com>
2018-08-14 13:07:25 +0100

    TEST create bank balance

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -1,1 +1,13 @@
+describe("Bank", () => {
+  var bank;
 
+  beforeEach(function() {
+    bank = new Bank();
+  });
+
+  describe(".balance", () => {
+    it("start at 0 ", () => {
+      expect(bank._balance).toEqual(0);
+    });
+  });
+});
